6df1b665935bba1da273e87cb4f76a1d4b8f5b45
jacksolaro <jacksolaro@gmail.com>
2020-11-05 11:47:51 -0800

    added more starter files

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">Pupster</h1>
+        <p className="lead">They're good boys and girls</p>
+      </div>
+      <div className="container">
+        <h3>Welcome to Pupster</h3>
+        <p>
+          I'm baby raclette lomo fixie letterpress fingerstache twee kombucha you probably haven't heard of them humblebrag viral dreamcatcher chambray. Migas banh mi letterpress poutine kale chips lomo. Tousled chartreuse taxidermy iPhone blue bottle sartorial kogi tattooed freegan dreamcatcher chia butcher. Church-key letterpress air plant schlitz vape chia scenester.
+
+          Typewriter blog art party craft beer humblebrag. Biodiesel af vinyl flexitarian retro readymade. Meggings thundercats gochujang locavore. Tumeric pickled post-ironic, banh mi edison bulb chambray heirloom fashion axe authentic fanny pack poutine bespoke cray. Synth vegan before they sold out adaptogen. Letterpress selvage slow-carb, heirloom marfa street art shabby chic offal. Blue bottle organic pitchfork pug raclette, marfa pour-over kickstarter viral hoodie wolf.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+function Search(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Search;
